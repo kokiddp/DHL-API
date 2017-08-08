d38aecd79359356f0cb4ef06183b01fca0ad1ede
kelvin <aquariuscool@gmail.com>
2017-08-08 11:51:26 +0800

    prevent error "Trying to get property of non-object"

--- DHL/Entity/Base.php
+++ DHL/Entity/Base.php
@@ -230,7 +230,7 @@ abstract class Base extends BaseDataType
     {
         $xml = simplexml_load_string(str_replace('req:', '', $xml));
 
-        if ((string) $xml->Response->Status->Condition->ConditionCode != '')
+        if ($xml->Response->Status && (string) $xml->Response->Status->Condition->ConditionCode != '')
         {
             $errorMsg = ((string) $xml->Response->Status->Condition->ConditionCode) . ' : ' . ((string) $xml->Response->Status->Condition->ConditionData);
             throw new \Exception('Error returned from DHL webservice : ' . $errorMsg);
